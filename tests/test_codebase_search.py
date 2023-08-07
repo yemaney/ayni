b1782ca40e4e00c1c56300be2e9d17955a36e4b3
yemaney <yemane_@outlook.com>
2023-08-07 21:03:55 +0000

    test(find_all_code): pass str for exts parameter

--- tests/test_codebase_search.py
+++ tests/test_codebase_search.py
@@ -2,5 +2,5 @@ from ayni.codebase_search import find_all_code
 
 
 def test_find_all_code_python(test_directory):
-    for fp in find_all_code(test_directory, [".py"]):
+    for fp in find_all_code(test_directory, ".py"):
         assert fp.endswith(".py")
